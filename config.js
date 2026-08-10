88f8261c52e21fe2fdb28e2b4fd52718b69eb9d7
LilEdelweissValero <liledelweissvalero@gmail.com>
2026-08-10 15:12:53 +0800

    Added Closed from

---
+++ config.js
@@ -0,0 +1,3 @@
+// Supabase credentials — DO NOT commit this file to git
+const SUPABASE_URL = 'https://kuukcwoetjvouieafzal.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dWtjd29ldGp2b3VpZWFmemFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzMzI3ODgsImV4cCI6MjEwMTkwODc4OH0.mHoe73qQpy75PzP-Ch9JJhD_QfuBkE1WjbbUk22iZTg';
